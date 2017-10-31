5bf3caf161a6f4d1fff1cd9091935508ad6fd95c
Christopher Fahey <cpfahey@aep.com>
2017-10-31 00:07:36 -0400

    unregistered service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { unregister } from './registerServiceWorker';
-unregister();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
+//registerServiceWorker();
